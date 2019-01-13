9878e6eed618a0d1cf597a60accdeacde33c929e
ps3 <psthree@aol.com>
2019-01-13 12:21:11 -0500

    Add POST/todos route an refactor mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 // which promise library mongoose uses
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//had to add , { useNewUrlParser: true }
+mongoose.connect(
+  'mongodb://localhost:27017/TodoApp',
+  { useNewUrlParser: true }
+);
 
 module.exports = {
   mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -2,9 +2,44 @@
 // cd /Users/peterstema/mongo/bin
 // ./mongod --dbpath ~/mongo-data
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+
+const app = express();
+
+//middle ware
+//turns body into json
+app.use(bodyParser.json());
+
+//routes(url and callback)
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  //saves to datebase and has callback for success, error
+  todo.save().then(
+    document => {
+      console.log(document);
+      //sends the doc back to user
+      res.send(document);
+    },
+    error => {
+      console.log('error: ', error);
+      //send back a status code and error
+      res.status(400).send(error);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Starting up on port 3000 sir');
+});
 
 // let newTodo = new Todo({
 //   text: '    test   '
